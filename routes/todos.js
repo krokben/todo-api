786706fc296a92db400f6239a37494534c9e3416
Tommy Svensson <tommykajsvensson@gmail.com>
2018-07-03 18:50:42 +0200

    change endpoints and add put endpoint

--- routes/todos.js
+++ routes/todos.js
@@ -10,10 +10,10 @@ router.get('/', (req, res) => {
   Todo.find().then(todos => res.json(todos));
 });
 
-// @route  POST todos/add
+// @route  POST todos
 // @desc   Add a todo
 // @access Public
-router.post('/add', (req, res) => {
+router.post('/', (req, res) => {
   const item = new Todo({
     text: req.body.text,
     category: req.body.category,
@@ -22,10 +22,23 @@ router.post('/add', (req, res) => {
   item.save().then(todo => res.json(todo));
 });
 
-// @route   DELETE todos/delete/:id
+// @route  PUT todos/:id
+// @desc   Change property of todo
+// @access Public
+router.put('/:id', (req, res) => {
+  if (req.body.property !== '_id') {
+    Todo.findByIdAndUpdate(req.params.id, req.body.changes)
+    .then(() => res.json({ success: true }))
+    .catch(err => res.status(404).json({ success: false }));
+  } else {
+    res.status(404).json({ success: false });
+  }
+});
+
+// @route   DELETE todos/:id
 // @desc    Delete a todo
 // @access  Public
-router.delete('/delete/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   Todo.findById(req.params.id)
     .then(todo => todo.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
